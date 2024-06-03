d1d715fa3ec0ff8d316cb2ff747ff53524dd590e
Youssef Waleed <ywsoliman@gmail.com>
2024-06-03 21:23:34 +0300

    Implemented the network service generic function

---
+++ ShopifyApp/Service/NetworkService/NetworkService.swift
@@ -0,0 +1,58 @@
+//
+//  NetworkService.swift
+//  ShopifyApp
+//
+//  Created by Youssef Waleed on 02/06/2024.
+//
+
+import Foundation
+import Alamofire
+
+enum APIError: Error {
+    case invalidURL
+    case requestFailed(Error)
+    case decodingFailed(Error)
+}
+
+struct NetworkService: NetworkServiceProtocol {
+    
+    static let shared = NetworkService()
+    private let BASE_URL = "https://mad44-sv-ios3.myshopify.com/admin/api/2024-04"
+    
+    private init() {}
+    
+    func makeRequest<T: Decodable>(
+        endPoint: String,
+        method: HTTPMethod,
+        parameters: Parameters? = nil,
+        headers: HTTPHeaders = ["X-Shopify-Access-Token": APIKey],
+        completion: @escaping (Result<T, APIError>) -> Void
+    ) {
+        
+        let urlWithEndPoint = BASE_URL + endPoint
+        guard let url = URL(string: urlWithEndPoint) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        AF.request(url, method: method, parameters: parameters, headers: headers)
+            .validate()
+            .responseData { response in
+                
+                switch response.result {
+                case .success(let data):
+                    do {
+                        let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+                        completion(.success(decodedResponse))
+                    } catch let decodingError {
+                        completion(.failure(.decodingFailed(decodingError)))
+                    }
+                case .failure(let error):
+                    completion(.failure(.requestFailed(error)))
+                    
+                }
+            }
+        
+    }
+    
+}
