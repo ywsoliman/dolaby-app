4ccbc8408c84361a65fb511624e0d99d31771f12
Samuel Adel <samueladel1001@gmail.com>
2024-06-09 04:24:14 +0300

    onboardiing done

--- ShopifyApp/Modules/Onboarding/View/OnboardingViewController.swift
+++ ShopifyApp/Modules/Onboarding/View/OnboardingViewController.swift
@@ -9,13 +9,48 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let viewModel:OnboardingViewModel = OnboardingViewModel()
+    var currentPageIndex = 0 {
+        didSet{
+            pageControl.currentPage = currentPageIndex
+            if currentPageIndex == viewModel.slides.count-1{
+                nextButton.setTitle("Start", for: .normal)
+            }else{
+                nextButton.setTitle("Next", for: .normal)
+            }
+            nextButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel.loadSlides()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        pageControl.numberOfPages = viewModel.slides.count
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func nextBtnClicked(_ sender: Any) {
 
+        if currentPageIndex < viewModel.slides.count-1 {
+            currentPageIndex += 1
+            let indexPath = IndexPath(item: currentPageIndex, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            
+        }else{
+            let controller = storyboard?.instantiateViewController(identifier: "loginNav") as! UINavigationController
+            controller.modalPresentationStyle = .fullScreen
+            controller.modalTransitionStyle = .flipHorizontal
+            present(controller, animated: true)
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -26,4 +61,25 @@ class OnboardingViewController: UIViewController {
     }
     */
 
+}
+extension OnboardingViewController : UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.slides.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnBoardingCollectionViewCell.identifier, for: indexPath) as! OnBoardingCollectionViewCell
+        cell.setup(viewModel.slides[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+        currentPageIndex = Int(scrollView.contentOffset.x / width)
+    }
 }
