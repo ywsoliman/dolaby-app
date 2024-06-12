7ef239ce41f4675a9f74a214b75ffaf8bc253846
Youssef Waleed <ywsoliman@gmail.com>
2024-06-12 15:16:19 +0300

    fix: A bug in storing currency value in UserDefaults

--- ShopifyApp/Modules/Settings/Currency/CurrencyManager.swift
+++ ShopifyApp/Modules/Settings/Currency/CurrencyManager.swift
@@ -15,7 +15,7 @@ struct CurrencyManager {
             UserDefaults.standard.set(currency, forKey: "currency")
         }
     }
-    static var value: Double = UserDefaults.standard.value(forKey: "currency") as? Double ?? 1.0 {
+    static var value: Double = UserDefaults.standard.value(forKey: "currencyValue") as? Double ?? 1.0 {
         didSet {
             UserDefaults.standard.set(value, forKey: "currencyValue")
         }
