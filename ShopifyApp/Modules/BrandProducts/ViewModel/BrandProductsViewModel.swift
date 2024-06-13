c55d99bc17d9e481cc1221b1b80c56b54d1068d4
Israa Assem <>
2024-06-13 18:42:55 -0400

    create categoriesViewModel

---
+++ ShopifyApp/Modules/BrandProducts/ViewModel/BrandProductsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BrandProductsViewModel.swift
+//  ShopifyApp
+//
+//  Created by Israa Assem on 13/06/2024.
+//
+
+import Foundation
---
+++ ShopifyApp/Modules/Categories/ViewModel/CategoriesViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  CategoriesViewModel.swift
+//  ShopifyApp
+//
+//  Created by Israa Assem on 10/06/2024.
+//
+
+import Foundation
+protocol CategoriesViewModelProtocol{
+    func getProducts()->[Product]
+    func getProductsCount()->Int
+    var  bindProductsToViewController:()->Void { get set }
+    func fetchProducts()->Void
+   
+}
+class CategoriesViewModel:CategoriesViewModelProtocol{
+    
+    let networkService:NetworkService
+    init(networkService: NetworkService) {
+        self.networkService = networkService
+    }
+    var bindProductsToViewController:()->Void={}
+    var allProducts:[Product]?=nil
+    
+
+    func fetchProducts(){
+        networkService.makeRequest(endPoint: "/products.json", method: .get) {[weak self] (result: Result<ProductsResponse, APIError>) in
+            switch result {
+            case .success(let response):
+                self?.allProducts=response.products
+                self?.filterProducts()
+                self?.bindProductsToViewController()
+            case .failure(let error):
+                print("Error in retrieving categories products: \(error)")
+            }
+        }
+    }
+    func getProducts()->[Product]{
+        return allProducts ?? []
+    }
+    func getProductsCount()->Int{
+        return allProducts?.count ?? 0
+    }
+   
+    
+   
+}
+
