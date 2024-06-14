3ddc284434f2a0dd3c6dcc2537fca3c69d06e6d1
Youssef Waleed <ywsoliman@gmail.com>
2024-06-14 15:52:50 +0300

    Tested fetching discount codes

--- ShopifyAppTests/NetworkServiceTests/Mock/NetworkServiceMock.swift
+++ ShopifyAppTests/NetworkServiceTests/Mock/NetworkServiceMock.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+@testable import ShopifyApp
+
+enum NetworkError: Error {
+    case genericError
+}
+
+struct NetworkServiceMock {
+    
+    var shouldFail: Bool = false
+    
+    private let fakeDiscountCodes: [String: Any] = [
+        "price_rules": [
+            [
+                "id": 1436614164780,
+                "value_type": "percentage",
+                "value": "-50.0",
+                "title": "RBQ484Y5MAQM"
+            ]
+        ]
+    ]
+    
+    func getDiscountCodes(completion: @escaping (PriceRulesResponse?, Error?) -> Void) {
+        
+        var priceRuleResponse: PriceRulesResponse?
+
+        do {
+            let data = try JSONSerialization.data(withJSONObject: fakeDiscountCodes)
+            priceRuleResponse = try JSONDecoder().decode(PriceRulesResponse.self, from: data)
+        } catch {
+            print("Mock error \(error)")
+        }
+        
+        shouldFail ? completion(nil, NetworkError.genericError) : completion(priceRuleResponse, nil)
+        
+    }
+    
+}
