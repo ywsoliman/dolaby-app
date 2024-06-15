b08e7bd3f497e6cb6e57dc557c2b8587c6595bea
Israa Assem <>
2024-06-15 18:35:38 -0400

    Fetch customer orders

--- ShopifyApp/Modules/Orders/View/OrdersViewController.swift
+++ ShopifyApp/Modules/Orders/View/OrdersViewController.swift
@@ -8,27 +8,28 @@
 import UIKit
 
 class OrdersViewController: UIViewController {
-
+    var ordersViewModel: OrdersViewModelProtocol?
+    let indicator = UIActivityIndicatorView(style: .large)
     @IBOutlet weak var ordersTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         let cellNib=UINib(nibName: "OrdersTableViewCell", bundle: nil)
         ordersTable.delegate=self
         ordersTable.dataSource=self
         ordersTable.register(cellNib, forCellReuseIdentifier: "ordersCell")
+        indicator.startAnimating()
+        ordersViewModel = OrdersViewModel(service: NetworkService.shared)
+        ordersViewModel?.fetchOrders()
+        ordersViewModel?.bindOrdersToViewController={[weak self] in
+            DispatchQueue.main.async {
+                self?.indicator.stopAnimating()
+                self?.ordersTable.reloadData()
+            }
+        }
+        view.addSubview(indicator)
+        indicator.center = self.view.center
+        indicator.startAnimating()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 extension OrdersViewController:UITableViewDelegate{
@@ -36,11 +37,13 @@ extension OrdersViewController:UITableViewDelegate{
 }
 extension OrdersViewController:UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        3
+        ordersViewModel?.getOrdersCount() ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell=tableView.dequeueReusableCell(withIdentifier: "ordersCell", for: indexPath) as! OrdersTableViewCell
+        cell.orderPrice.text=ordersViewModel?.getOrders()[indexPath.row].currentTotalPrice
+        cell.orderDate.text=ordersViewModel?.getOrders()[indexPath.row].createdAt
         return cell
     }
     
--- ShopifyApp/Modules/Orders/ViewModel/OrdersViewModel.swift
+++ ShopifyApp/Modules/Orders/ViewModel/OrdersViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+protocol OrdersViewModelProtocol{
+    func getOrders()->[Order]
+    func getOrdersCount()->Int
+    var bindOrdersToViewController:()->Void { get set }
+    func fetchOrders()->Void
+}
+class OrdersViewModel:OrdersViewModelProtocol{
+    
+    private let service: NetworkService
+    var bindOrdersToViewController:()->Void={}
+    var Orders:[Order]?=nil
+    init(service: NetworkService) {
+        self.service = service
+    }
+    func fetchOrders(){
+        service.makeRequest(endPoint: "/customers/\(CurrentUser.user?.id ?? 8136296333612)/orders.json", method: .get) {[weak self] (result: Result<CustomerOrdersResponse, APIError>) in
+            switch result {
+            case .success(let response):
+                self?.Orders=response.orders
+                self?.bindOrdersToViewController()
+            case .failure(let error):
+                print("Error in retrieving orders: \(error)")
+            }
+        }
+    }
+    func getOrders()->[Order]{
+        return Orders ?? []
+    }
+    func getOrdersCount()->Int{
+        return Orders?.count ?? 0
+    }
+}
