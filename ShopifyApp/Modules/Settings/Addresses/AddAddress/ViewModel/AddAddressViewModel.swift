9cc7d744d4d1fb9928aecc3dae3af0fbdb2542d6
Youssef Waleed <ywsoliman@gmail.com>
2024-06-18 20:11:39 +0300

    fix: A bug in addresses

--- ShopifyApp/Modules/Settings/Addresses/AddAddress/ViewModel/AddAddressViewModel.swift
+++ ShopifyApp/Modules/Settings/Addresses/AddAddress/ViewModel/AddAddressViewModel.swift
@@ -57,7 +57,7 @@ class AddAddressViewModel: NSObject, CLLocationManagerDelegate {
         service.makeRequest(endPoint: "/customers/\(user.id)/addresses.json", method: .post, parameters: addressParams) { (result: Result<CustomerAddress, APIError>) in
             
             switch result {
-            case .success(let address):
+            case .success:
                 self.bindAddressToViewController()
             case .failure(let error):
                 self.bindInvalidCountryToViewController()
