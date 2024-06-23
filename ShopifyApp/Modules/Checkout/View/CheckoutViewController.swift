84ca9df48f01f5ee03c25c15f5890c9e57c82491
Youssef Waleed <ywsoliman@gmail.com>
2024-06-23 17:24:44 +0300

    Limit the characters of promo text field

--- ShopifyApp/Modules/Checkout/View/CheckoutViewController.swift
+++ ShopifyApp/Modules/Checkout/View/CheckoutViewController.swift
@@ -31,6 +31,9 @@ class CheckoutViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        promoTextField.delegate = self
+        
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(CartTableViewCell.nib(), forCellReuseIdentifier: CartTableViewCell.identifier)
@@ -314,5 +317,19 @@ extension CheckoutViewController: PKPaymentAuthorizationViewControllerDelegate {
         }
     }
     
+    
+}
+
+extension CheckoutViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let maxLength = 15
+        let currentString = (textField.text ?? "") as NSString
+        let newString = currentString.replacingCharacters(in: range, with: string)
+
+        return newString.count <= maxLength
+        
+    }
     
 }
