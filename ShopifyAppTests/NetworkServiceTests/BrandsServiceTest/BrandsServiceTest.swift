c0fec9049d1ae595d8f8ea8993e8abae29c8c5eb
Israa Assem <>
2024-06-24 03:10:11 -0400

    Add expextation tests for brands service

--- ShopifyAppTests/NetworkServiceTests/BrandsServiceTest/BrandsServiceTest.swift
+++ ShopifyAppTests/NetworkServiceTests/BrandsServiceTest/BrandsServiceTest.swift
@@ -6,30 +6,75 @@
 //
 
 import XCTest
-
+@testable import ShopifyApp
 final class BrandsServiceTest: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testFetchingBrands() {
+        
+        let expectation = expectation(description: "Waiting for API")
+        
+        NetworkService.shared.makeRequest(endPoint: "/smart_collections.json", method: .get) { (result: Result<BrandsResponse, APIError>) in
+            
+            switch result {
+            case .success(let response):
+                XCTAssertNotNil(response)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Request failed with error: \(error)")
+                expectation.fulfill()
+            }
+            
         }
+        
+        waitForExpectations(timeout: 5)
+        
     }
-
+    func testNumberOfFetchedBrands() {
+        
+        let expectation = expectation(description: "Waiting for API")
+        
+        NetworkService.shared.makeRequest(endPoint: "/smart_collections.json", method: .get) { (result: Result<BrandsResponse, APIError>) in
+            
+            switch result {
+            case .success(let response):
+                XCTAssertEqual(response.brands.count,12)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Request failed with error: \(error)")
+                expectation.fulfill()
+            }
+            
+        }
+        
+        waitForExpectations(timeout: 5)
+        
+    }
+    func testFirstBrandId() {
+        
+        let expectation = expectation(description: "Waiting for API")
+        
+        NetworkService.shared.makeRequest(endPoint: "/smart_collections.json", method: .get) { (result: Result<BrandsResponse, APIError>) in
+            
+            switch result {
+            case .success(let response):
+                XCTAssertEqual(response.brands.first?.id,475723497772)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Request failed with error: \(error)")
+                expectation.fulfill()
+            }
+            
+        }
+        
+        waitForExpectations(timeout: 5)
+        
+    }
 }
