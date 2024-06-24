efd50f1a89f500119cf42bb89f4e5986aea62628
Israa Assem <>
2024-06-24 09:43:19 -0400

    Add expectation tests for orders service

--- ShopifyApp/Models/ProductsResponse.swift
+++ ShopifyApp/Models/ProductsResponse.swift
@@ -43,50 +43,6 @@ struct CategoriesProduct: Codable {
         case variants, image
     }
 }
-//
-//struct Variant: Codable {
-//    let id: Int64
-//    let productID: Int64
-//    let title, price, sku: String
-//    let position: Int
-//    let inventoryPolicy: String
-//    let compareAtPrice: String?
-//    let fulfillmentService, inventoryManagement: String
-//    let option1, option2, option3: String?
-//    let createdAt, updatedAt: String
-//    let taxable: Bool
-//    let barcode: String?
-//    let grams: Int
-//    let weight: Double
-//    let weightUnit: String
-//    let inventoryItemID: Int64
-//    let inventoryQuantity, oldInventoryQuantity: Int
-//    let requiresShipping: Bool
-//    let adminGraphqlAPIID: String
-//    let imageID: Int?
-//
-//    enum CodingKeys: String, CodingKey {
-//        case id
-//        case productID = "product_id"
-//        case title, price,sku
-//        case  position
-//        case inventoryPolicy = "inventory_policy"
-//        case compareAtPrice = "compare_at_price"
-//        case fulfillmentService = "fulfillment_service"
-//        case inventoryManagement = "inventory_management"
-//        case option1, option2, option3
-//        case createdAt = "created_at"
-//        case updatedAt = "updated_at"
-//        case taxable, barcode, grams, weight
-//        case weightUnit = "weight_unit"
-//        case inventoryItemID = "inventory_item_id"
-//        case inventoryQuantity = "inventory_quantity"
-//        case oldInventoryQuantity = "old_inventory_quantity"
-//        case requiresShipping = "requires_shipping"
-//        case adminGraphqlAPIID = "admin_graphql_api_id"
-//        case imageID = "image_id"
-//    }
-//}
 
 struct ProductOption: Codable {
     let id: Int64
---
+++ ShopifyAppTests/NetworkServiceTests/OrdersServiceTests/Mock/OrdersServiceMock.swift
@@ -0,0 +1,8 @@
+//
+//  OrdersServiceMock.swift
+//  ShopifyAppTests
+//
+//  Created by Israa Assem on 23/06/2024.
+//
+
+import Foundation
